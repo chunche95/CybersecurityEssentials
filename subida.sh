aedee21934d6ff85451d00d489bec5a56f0ad5ed
chunche95 <paeste95.pb@gmail.com>
2020-03-15 21:20:21 +0100

    Script subida archivos

---
+++ subida.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+git add assets/css/. 
+git nota "Styles"
+git sube
+
+git add assets/fonts/.
+git nota "Fonts"
+git sube
+
+git add assets/js/.
+git nota "JS"
+git sube
+
+git add assets/sass/.
+git nota "SCSS"
+git sube
+
+git add images/fulls/.
+git nota "Img. Fulls"
+git sube
+
+git add images/thumbs/. 
+git nota "Img. Thumbs"
+git sube 
+
+git add images/.
+git nota "Images"
+git sube
+
+git add index.html
+git nota "Introducction Cybersecurity - NetAcademy."
+git sube
+
+git add subida.sh
+git nota "Script subida archivos"
+git sube
