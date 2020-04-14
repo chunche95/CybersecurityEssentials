587a431d2626baf079883b554a2f4a59b6bea216
chunche95 <paeste95.pb@gmail.com>
2020-04-15 00:54:33 +0200

    Script - Update Folderes

--- subida.sh
+++ subida.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# +-------------------------------------------+
+# |----------------- ASSET   -----------------|
+# +-------------------------------------------+
+
 git add assets/css/. 
 git nota "Styles"
 git sube
@@ -16,26 +20,7 @@ git add assets/sass/.
 git nota "SCSS"
 git sube
 
-git add images/fulls/.
-git nota "Img. Fulls"
-git sube
-
-git add images/thumbs/. 
-git nota "Img. Thumbs"
-git sube 
-
-git add images/.
-git nota "Images"
-git sube
-
-git add index.html
-git nota "Introduction Cybersecurity - NetAcademy."
-git sube
-
-
-git add subida.sh
-git nota "Script - Update Folderes"
-git sube
+# -------------- cybersecurity.
 
 git add cybersecurityEssentials/assets/css/.
 git nota "Style Contents"
@@ -49,13 +34,73 @@ git add cybersecurityEssentials/assets/js/.
 git nota "JS contents"
 git sube
 
+# -------------- cybersecurity.
+
 git add cybersecurityEssentials/assets/sass/.
 git nota "Sass contents"
 git sube
 
+
+
+# +-----------------------------------------------+
+# |----------------- DOCUMENTOS  -----------------|
+# +-----------------------------------------------+
+
+# ------------- VIDEOS
+git add cybersecurityEssentials/videos/.
+git nota "Videos"
+git sube
+# ------------- DOCUMENTOS
+git add cybersecurityEssentials/docs/.
+git nota "Documents"
+git sube
+# ------------- GUÍA
+git add Hackers-Guide-to-Machine-Learning-with-Python/.
+git nota "Guide to ML Hacking"
+git sube
+
+# +--------------------------------------------+
+# |----------------- IMAGES   -----------------|
+# +--------------------------------------------+
+
+git add images/fulls/.
+git nota "Img. Fulls"
+git sube
+
+git add images/thumbs/. 
+git nota "Img. Thumbs"
+git sube 
+
+git add images/.
+git nota "Images"
+git sube
+
+
 git add cybersecurityEssentials/images/.
 git nota "Images to contents"
 git sube
+
+# +-------------------------------------------+
+# |----------------- INDEX   -----------------|
+# +-------------------------------------------+
+
+git add index.html
+git nota "Introduction Cybersecurity - NetAcademy."
+git sube
+
+# +-------------------------------------------+
+# |----------------- SCRIPT  -----------------|
+# +-------------------------------------------+
+
+
+git add subida.sh
+git nota "Script - Update Folderes"
+git sube
+
+
+# +---------------------------------------------+
+# |----------------- CAPÍTULOS  ----------------|
+# +---------------------------------------------+
 
 git add cybersecurityEssentials/cap1.html
 git nota "Capítulo 1 -  La ciberseguridad"
@@ -65,14 +110,3 @@ git add cybersecurityEssentials/cap2.html
 git nota "Capítulo 2 - Destrezas de la ciberseguridad"
 git sube
 
-git add cybersecurityEssentials/videos/.
-git nota "Videos"
-git sube
-
-git add cybersecurityEssentials/docs/.
-git nota "Documents"
-git sube
-
-git add Hackers-Guide-to-Machine-Learning-with-Python/.
-git nota "Guide to ML Hacking"
-git sube
